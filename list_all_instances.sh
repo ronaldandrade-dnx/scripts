42b68729a145ea7bfb7f26a4fa976f08971e785d
ronaldandrade-dnx <ronald.andrade@dnxbrasil.com>
2026-01-20 16:29:09 -0300

    Add script to list all EC2 instances by region

---
+++ list_all_instances.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Cabeçalho
+echo "Nome,ID,Tipo,Região"
+
+# Lista todas as regiões disponíveis
+REGIONS=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+
+for REGION in $REGIONS; do
+  aws ec2 describe-instances \
+    --region "$REGION" \
+    --query 'Reservations[].Instances[].[
+      Tags[?Key==`Name`].Value | [0],
+      InstanceId,
+      InstanceType
+    ]' \
+    --output text | while read NAME ID TYPE; do
+      echo "${NAME:-SemNome},$ID,$TYPE,$REGION"
+    done
+done
